4335d4f8ec2be06779920f613d5494d5709336fb
JooHyung Lee <yamiblack95@gmail.com>
2022-02-05 17:45:24 +0900

    Initial commit

---
+++ app/src/main/java/com/study/android/now/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.study.android.now.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
